b7984702a746e262c011561a740bf122386b5d76
Davide Molitierno <>
2023-07-24 17:32:32 +0200

    Terminato top10 playlists per group tracks

--- analytics/top10group_tracks_playlist.py
+++ analytics/top10group_tracks_playlist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # top 10 playlist per gruppo di 5 tracks (ricorda di calcolare il gruppo all’interno della singola playlist)
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, collect_set, row_number, desc, col
@@ -17,9 +17,9 @@ parser.add_argument("--output", help="the output directory path", type=str)
 spark = SparkSession. \
             builder. \
             config("spark.driver.host", "localhost"). \
-            config("spark.executor.memory", "8g"). \
-            config("spark.storage.memoryFraction", "0"). \
-            config("shuffle.memoryFraction", "0"). \
+            config("spark.driver.memory", "16g"). \
+            config("spark.executor.memory", "16g"). \
+            config("spark.sql.broadcastTimeout", "36000"). \
             appName("Top10Track_playlists"). \
             getOrCreate()
 
@@ -36,25 +36,27 @@ tracks_df = playlist_df.withColumn("track", explode("tracks"))
 
 # calculate every group of 5 tracks for each playlist
 # group by playlist and calculate all the track_names in the playlist
-playlist_df = tracks_df.groupBy("name").agg(collect_set("track.track_name").alias("track_names")) \
-                        .join(tracks_df, "name").select("name", "num_followers", "track_names")
+playlist_df = tracks_df.groupBy("name", "num_followers").agg(collect_set("track.track_name").alias("track_names"))
 
 # UDF to calculate the group of 5 tracks
 # con 5 crasha
 @udf(ArrayType(ArrayType(StringType())))
 def calculate_group(tracks):
     group = []
-    group.extend(itertools.combinations(tracks, 2))
+    group.extend(itertools.combinations(tracks, 3))
     return group
 
-# # calculate the group of 5 tracks for each playlist
-playlist_df = playlist_df.repartition("name") \
-                         .withColumn("group", explode(calculate_group("track_names")))
+# calculate the group of 5 tracks for each playlist
+playlist_df = playlist_df.withColumn("group", explode(calculate_group("track_names"))) \
+                         .select("group", "name", "num_followers")
+
 
 # get the top 10 playlists for each group of 5 tracks ordered by num_followers
-grouped_playlist_df = playlist_df.repartition("group") \
-                                    .withColumn("rank", row_number().over(Window.partitionBy("group").orderBy(desc("num_followers")))) \
-                                    .where(col("rank") <= 10) \
+window_spec = Window.partitionBy("group").orderBy(desc("num_followers"))
 
+grouped_playlist_df = playlist_df.withColumn("rank", row_number().over(window_spec)).where(col("rank") <= 10)
+
+# order by group
+grouped_playlist_df = grouped_playlist_df.orderBy("group")
 # write the output file
 grouped_playlist_df.write.json(output_dir + "/top10group_tracks_playlist.json", mode="overwrite")
