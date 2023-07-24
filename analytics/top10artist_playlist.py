2e112b6b42d5fdc620e182e55196a1283d8f8ecf
davidegattini <davidegatto99@gmail.com>
2023-07-24 17:32:47 +0200

    Aggiunta analisti per prendere le top playlist per artista (batch)

---
+++ analytics/top10artist_playlist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# top 10 playlist per ciascun artist
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, desc, col, row_number
+from pyspark.sql.window import Window
+
+import argparse
+
+# create argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="the input directory or file path", type=str)
+parser.add_argument("--output", help="the output directory path", type=str)
+
+# create spark session
+spark = SparkSession. \
+            builder. \
+            config("spark.driver.host", "localhost"). \
+            config("spark.executor.memory", "8g"). \
+            config("spark.storage.memoryFraction", "0.2"). \
+            appName("Top10Artist_playlists"). \
+            getOrCreate()
+
+# parse the arguments
+args = parser.parse_args()
+input_file = args.input
+output_dir = args.output
+
+# read the input file
+playlist_df = spark.read.json(input_file) \
+                    .cache()
+
+# for each artist get all the playlists in which it appears
+artist_df = playlist_df.withColumn("artist_name", explode("tracks.artist_name"))
+
+window = Window.partitionBy("artist_name").orderBy(desc("num_followers"))
+
+artist_df = artist_df.select("artist_name", "name", "num_followers")
+
+# get the top 10 playlists for each artist ordered by num_followers
+top10artist_playlist_df = artist_df.withColumn("rank", row_number().over(window)) \
+                                    .where(col("rank") <= 10) 
+
+# order by artist_name
+top10artist_playlist_df.orderBy("artist_name")
+
+top10artist_playlist_df.write.json(output_dir + "/top10artist_playlist.json")
--- consumer/streaming_consumer.py
+++ consumer/streaming_consumer.py
@@ -1,40 +1,49 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType
+from pyspark.sql.functions import col, from_json, explode, max
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType
 
 # define spark session
 spark = SparkSession.builder.appName('StreamingConsumer').getOrCreate()
 
 # read stream from kafka using spark
-lines = spark.readStream.format('kafka').option('kafka.bootstrap.servers', 'localhost:9092').option('subscribe', 'new_playlists').load()
+lines = spark.readStream.format('kafka').option('kafka.bootstrap.servers', 'localhost:9092').option('subscribe', 'json-topic').load()
 
 # define schema for the streaming data
 schema = StructType([
-            StructField("playlists", ArrayType(StructType([
                 StructField("name", StringType()),
                 StructField("collaborative", StringType()),
-                StructField("pid", StringType()),
-                StructField("modified_at", StringType())
-            ]))),
-            StructField("tracks", ArrayType(StructType([
-                StructField("pos", StringType())
-            ])))]
-        )
+                StructField("pid", IntegerType()),
+                StructField("modified_at", StringType()),
+                StructField("num_tracks", IntegerType()),
+                StructField("num_albums", IntegerType()),
+                StructField("num_followers", IntegerType()),
+                StructField("tracks", ArrayType(StructType([
+                    StructField("pos", IntegerType()),
+                    StructField("artist_name", StringType()),
+                    StructField("track_uri", StringType()),
+                    StructField("artist_uri", StringType()),
+                    StructField("track_name", StringType()),
+                    StructField("album_uri", StringType()),
+                    StructField("duration_ms", IntegerType()),
+                    StructField("album_name", StringType())
+                ])))
+            ])
 
-# QUERY DEFINITION
 # convert the data from binary to string and then to json
 # kafka stream data are composed of key, value, topic, partition, offset, timestamp, timestampType
-# TODO: capisci cosa è la chiave
-out = lines.withColumn("value", from_json(col("value").cast("string"), schema=schema)).select("value")
+# then select the parsed value and expand it to columns
+data = lines.select(from_json(col("value").cast("string"), schema).alias("parsed_value")).select(col("parsed_value.*"))
+data = data.withColumn("track", explode("tracks")).select("track.artist_name", "name", "num_followers")
 
-# data preparation
-out2 = out.select("value.playlists.name", "value.playlists.collaborative")
+out2 = data.groupBy("artist_name").max("num_followers")
 
-# data processing
-# TODO: processamento e analisi dei dati
+dataStreamWriter = out2.writeStream.format('console').outputMode('complete')
 
-# QUERY EXECUTION
-qr1 = out2.writeStream.format('console').start()
+# Query execution
+query = dataStreamWriter.start()
+
+# Query explanation
+# query.explain()
 
 # To avoid termination while streaming data continues arriving
-qr1.awaitTermination()
+query.awaitTermination()
